a22b1d782c707180cc949c19ad93753b81c51b20
AngrySquirrell <guigui.maugin@gmail.com>
2026-03-03 14:54:02 +0100

    feat: initialize pixelarena shell application with module federation
    - Added package.json for project dependencies and scripts.
    - Created index.html as the entry point for the application.
    - Implemented App.css for styling the application layout.
    - Developed App.jsx to load remote components using React's Suspense.
    - Set up bootstrap.jsx to render the main App component.
    - Created index.js to initiate the application.
    - Configured webpack for module federation and development server.
    - Added start.cmd script to facilitate starting all micro-frontends.

---
+++ pixelarena-checkpoint6/start.cmd
@@ -0,0 +1,18 @@
+cd ./mfe-cart &&
+npm i && npm start 
+&&
+
+cd ../mfe-catalog &&
+npm i && npm start 
+&&
+
+cd ../mfe-header &&
+npm i && npm start 
+&&
+
+cd ../mfe-lobby &&
+npm i && npm start 
+&&
+
+cd ../mfe-shell &&
+npm i && npm start 
