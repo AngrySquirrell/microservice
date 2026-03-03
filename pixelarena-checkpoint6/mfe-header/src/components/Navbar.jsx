03f2329f201b0b48ff06080133d2c7d59a777812
AngrySquirrell <guigui.maugin@gmail.com>
2026-03-03 14:57:23 +0100

    feat: implémentation de la gestion des notifications dans la barre de navigation

--- pixelarena-checkpoint6/mfe-header/src/components/Navbar.jsx
+++ pixelarena-checkpoint6/mfe-header/src/components/Navbar.jsx
@@ -1,30 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import eventBus from 'shared/eventBus';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import eventBus from "shared/eventBus";
+import "./Navbar.css";
 
 function Navbar() {
   const [notifications, setNotifications] = useState(0);
 
   useEffect(() => {
-    // TODO 1: S'abonner a l'evenement 'game:joined'
-    // Quand on recoit cet evenement, incrementer les notifications
-    //
-    // Indice: eventBus.on('nomEvenement', (data) => { ... })
-    //
-    // La fonction doit:
-    // 1. Incrementer notifications de 1 avec setNotifications
-    // 2. Optionnel: afficher data.gameName dans la console
-    //
-    // Ecrivez votre code ici:
+    const unsubGameJoined = eventBus.on("game:joined", (data) => {
+      setNotifications((prev) => prev + 1);
+    });
 
+    const unsubCartUpdated = eventBus.on("cart:updated", (data) => {
+      setNotifications(data.count);
+    });
 
-    // TODO 2: Se desabonner quand le composant est demonte
-    // C'est IMPORTANT pour eviter les memory leaks !
-    //
-    // Indice: retourner une fonction cleanup dans useEffect
-    //
-    // return () => { ... };
-
+    return () => {
+      unsubGameJoined();
+      unsubCartUpdated();
+    };
   }, []);
 
   return (
